c8ab5ba24246725ea714ef5b3f86af6f6ff79e80
程义威 <1485868106@qq.com>
2023-11-13 19:19:04 +0800

    2023.11.13

--- a.cpp
+++ a.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    printf("Hello World!");
+    double v1,v2,V,a,PI=3.141593;
+        v1=PI*4*4*4*(4/3.0)*1.0;
+        v2=PI*10*10*10*(4/3.0)*1.0;
+        V=v1+v2;
+        a=pow(V,1.0/3);
+        printf("%.4lf",a);
     return 0;
 }
