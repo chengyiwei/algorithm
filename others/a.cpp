4842eda59c39dea46bcfbe0d732ef178c1597c09
程义威 <1485868106@qq.com>
2023-11-19 00:20:35 +0800

    11.18

--- others/a.cpp
+++ others/a.cpp
@@ -1,53 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
-int temp[2000005]={0};
-long long temp1[2000005]={0};
-long long temp2[2000005]={0};
-struct IO{
-    static const int S=1<<21;
-    char buf[S],*p1,*p2;int st[105],Top;
-    ~IO(){clear();}
-    inline void clear(){fwrite(buf,1,Top,stdout);Top=0;}
-    inline void pc(const char c){Top==S&&(clear(),0);buf[Top++]=c;}
-    inline char gc(){return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;}
-    inline IO&operator >> (char&x){while(x=gc(),x==' '||x=='\n'||x=='r');return *this;}
-    template<typename T>inline IO&operator >> (T&x){
-        x=0;bool f=0;char ch=gc();
-        while(ch<'0'||ch>'9'){if(ch=='-') f^=1;ch=gc();}
-        while(ch>='0'&&ch<='9') x=(x<<3)+(x<<1)+ch-'0',ch=gc();
-        f?x=-x:0;return *this;
-    }
-    inline IO&operator << (const char c){pc(c);return *this;}
-    template<typename T>inline IO&operator << (T x){
-        if(x<0) pc('-'),x=-x;
-        do{st[++st[0]]=x%10,x/=10;}while(x);
-        while(st[0]) pc('0'+st[st[0]--]);return *this;
-    }
-}fin,fout;
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-    int n,m;
-    fin>>n>>m;
-    for (int i=1;i<=n;i++)
+    char a[100]="";
+    int i,count=0;
+    gets(a);
+    for(i=0;a[i+1]!='\0';i++)
     {
-        fin>>temp[i];
-        if (i==1)
-        {
-            temp1[i]=temp[i];
-            temp2[i]=temp[i];
-        }
-        else
-        {
-            temp1[i]=temp1[i-1]+temp[i];
-            temp2[i]=(temp2[i-1]^temp[i]);
-        }
+        if((a[i]!=' '&&a[i+1]==' ')||(a[i]!=' '&&a[i+1]=='\n'))
+            count++;
     }
-    int a,l,r;
-    for (int i=0;i<m;i++)
-    {
-        fin>>a>>l>>r;
-        if (a==1) fout<<temp1[r]-temp1[l-1]<<'\n';
-        else fout<<(temp2[r]^temp2[l-1])<<'\n';
-    }
+    printf("%d",count);
     return 0;
 }
--- others/geometry.cpp
+++ others/geometry.cpp
@@ -5,10 +5,15 @@ struct Point{
     double x,y;
     Point(double x=0,double y=0):x(x),y(y){};
 };
-
-
 typedef Point Vector;
-
+struct Line{
+    Point P;
+    Vector v;
+    double ang;
+    Line(){}
+    Line(Point p,Vector v):P(p),v(v){ang=atan2(v.y,v.x);}
+    bool operator <(const Line& L) const{return ang<L.ang;}
+};
 Vector operator + (Vector A,Vector B) {return Vector{A.x+B.x,A.y+B.y};} //向量+向量=向量
 Vector operator - (Point A,Point B) {return Vector{A.x-B.x,A.y-B.y};} //点-点=向量
 Vector operator * (Vector A,double p) {return Vector{A.x*p,A.y*p};} //向量*数=向量
@@ -81,6 +86,77 @@ double ConvexPolgonArea(Point *p,int n){
         area += Cross(p[i]-p[0],p[i+1]-p[0]);
     return area/2;
 }
-int main(){
 
+typedef Point* Polygon;
+
+int isPointInPolygon(Point p,Polygon poly,int n){
+    int wn=0;
+    for(int i=0;i<n;i++){
+        if(OnSegment(p,poly[i],poly[(i+1)&n])) return -1;//在边界上
+        int k=dcmp(Cross(poly[(i+1)%n]-poly[i],p-poly[i]));
+        int d1=dcmp(poly[i].y-p.y);
+        int d2=dcmp(poly[(i+1)%n].y-p.y);
+        if(k>0&&d1<=0&&d2>0) wn++;
+        if(k<0&&d2<=0&&d1>0) wn--;
+    }
+    if(wn!=0) return 1; //内部
+    return 0; //外部
 }
+
+bool cmp(Point A,Point B){
+    return A.x<B.x||(dcmp(A.x-B.x)==0&&A.y<B.y);
+}
+int ConvexHull(Point* p,int n,Point* ch){ // 凸包
+    sort(p,p+n,cmp);
+    int m=0; 
+    for(int i=0;i<n;i++){ //下面一半
+        while(m>1 && Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<=0)m--;
+        ch[m++]=p[i];
+    }
+    int k=m;
+    for(int i=n-2;i>=0;i--){ //上面一半
+        while(m>k&&Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<=0) m--;
+        ch[m++]=p[i];
+    }
+    if(n>1)m--;
+    return m;
+}
+
+//点 p 在有向直线 L 的左边(线上不算)
+bool OnLeft(Line L,Point p){
+    return Cross(L.v,p-L.P)>0;
+}
+
+Point GetIntersction(Line a,Line b){//二直线交点，假定交点唯一存在
+    Vector u=a.P-b.P;
+    double t=Cross(b.v,u)/Cross(a.v,b.v);
+    return a.P+a.v*t;
+}
+
+int HalfplanIntersection(Line *L,int n,Point* poly){
+    sort(L,L+n); //按照极角排序
+
+    int first,last; //双端队列的第一个元素和最后一个元素的下标
+    Point *p=new Point[n]; //p[i] 为 q[i]和q[i+1]的交点
+    Line *q=new Line[n]; //双端队列
+    q[first=last=0]=L[0]; //双端队列初始化为只有一个半平面 L[0]
+    for(int i=1;i<n;i++){
+        while(first<last&&!OnLeft(L[i],p[last-1]))last--;
+        while(first<last&&!OnLeft(L[i],p[first])) first++;
+        q[++last]=L[i];
+        if(fabs(Cross(q[last].v,q[last-1].v))<eps) {//两个向量平行且相同，取内侧 的一个
+            last--;
+            if(OnLeft(q[last],L[i].P)) q[last]=L[i];
+        }
+        if(first<last) p[last-1]=GetIntersction(q[last-1],q[last]);
+    }
+    while(first<last&&!OnLeft(q[first],p[last-1]))last--;
+    if(last-first<=1) return 0;// 空集
+    p[last]=GetIntersction(q[last],q[first]); //计算首尾两个半平面的交点
+
+    //从deque复制到输出中
+    int m=0;
+    for(int i=first;i<=last;i++) poly[m++]=p[i];
+    return m;
+}
+int main(){}
