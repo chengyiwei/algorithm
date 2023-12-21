b838442ec3214f38058716524b07ec8215a84367
程义威 <1485868106@qq.com>
2023-12-21 12:57:58 +0800

    12.21

--- others/a.cpp
+++ others/a.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
+#include<cstring>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
+#include<vector>
+#include<string>
+
 using namespace std;
+
+int brand(){return (rand()<<15)|rand();}
+//st 学生名单 
+vector<string> st = {"神秘选手1号","神秘选手2号","陈皓轩", "程义威", "丁烨希", "方奕俊", "郭宇杰", "蓝玉婷", "李家诚", "李洋", "李之豪", "李志宇", "林立仁", "林谦", "刘爱平", "罗海波", "罗豪", "彭灿", "沈锐", "谭佳燕", "王立为", "肖薛林", "颜琪翊加", "张宇", "赵星如", "赵紫瑜", "周星同"};
+//black 黑名单 
+vector<string> black ={"神秘选手1号","神秘选手2号"};
+
 int main(){
-    int T;
-    scanf("%d%*c",&T);//%*c表示读入一个字符但不赋值
-    string s[1000],now_s;
-    for(int i=1;i<=T;i++)
-        getline(cin,s[i]);
-    getline(cin,now_s);
-    for(int i=1;i<=T;i++){
-        if(now_s==s[i]){
-            printf("%d\n",i);
-            return 0;
-        }
-    }
-    printf("-1\n");
+    srand(time(0));
+    int now=brand()%st.size();
+    while(find(black.begin(),black.end(),st[now])!=black.end())
+        now=brand()%st.size();
+    printf("%s",st[now].c_str());
     return 0;
 }
