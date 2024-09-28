43246593c49feaaac0fbe23aeb58a868d206f118
chengyiwei <1485868106@qq.com>
2024-09-28 23:38:07 +0800

    9.28

--- CDJCC_ACM/2024#113/A/A.cpp
+++ CDJCC_ACM/2024#113/A/A.cpp
@@ -5,7 +5,7 @@ int main() {
     int T; cin >> T;
     while (T--) {
         int a, b; cin >> a >> b; 
-        while (a > 2) a -= 2;
+        while (a > 2) a -= 2; qwq
         while (b >= 2) b -= 2;
         if (a == 0 && b == 0) cout << "YES\n";
         else if (a == 2 && b == 0) cout << "YES\n";
